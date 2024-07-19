f9cd81e913d622b236be34c703dd23f7ab790a96
Fran <francisco.perez@bolttech.io>
2024-07-19 16:50:21 +0200

    fix status code

--- app/AuthContext/Infrastructure/User/Http/CreateUserTokenController.php
+++ app/AuthContext/Infrastructure/User/Http/CreateUserTokenController.php
@@ -36,6 +36,10 @@ class CreateUserTokenController
             $request->password
         );
 
+        if ($response['error']) {
+            return new JsonResponse($response, Response::HTTP_UNAUTHORIZED);
+        }
+
         return new JsonResponse($response, Response::HTTP_OK);
     }
 }
