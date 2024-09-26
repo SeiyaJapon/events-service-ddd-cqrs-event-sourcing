00a177a7771613ad6bca6214ec59adfa927c7f71
Fran <francisco.perez@bolttech.io>
2024-09-26 19:04:20 +0200

    fix

--- app/EventContext/Application/Event/Commands/ImportEvents/ImportEventsCommandHandler.php
+++ app/EventContext/Application/Event/Commands/ImportEvents/ImportEventsCommandHandler.php
@@ -1,22 +1,19 @@
 <?php
 
-declare(strict_types=1);
+declare (strict_types = 1);
 
 namespace App\EventContext\Application\Event\Commands\ImportEvents;
 
-use App\EventContext\Infrastructure\Event\Persistence\Repository\DataThirdPartyEventRepository;
+use App\EventContext\Domain\Event\Repositories\ThirdPartyEventRepositoryInterface;
 
-class ImportEventsCommandHandler
-{
-    private DataThirdPartyEventRepository $repository;
+class ImportEventsCommandHandler {
+	private ThirdPartyEventRepositoryInterface $repository;
 
-    public function __construct(DataThirdPartyEventRepository $repository)
-    {
-        $this->repository = $repository;
-    }
+	public function __construct(ThirdPartyEventRepositoryInterface $repository) {
+		$this->repository = $repository;
+	}
 
-    public function handle(ImportEventsCommand $command): void
-    {
-        $this->repository->import($command->getUrl());
-    }
+	public function handle(ImportEventsCommand $command): void {
+		$this->repository->import($command->getUrl());
+	}
 }
